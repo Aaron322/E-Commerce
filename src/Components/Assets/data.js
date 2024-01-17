7fe09f907ddb31c8bcf23a67d804a5abf8bcdbdd
Aaron <2000huhaoyu@gmail.com>
2024-01-17 23:12:05 +1100

    update some test data and to see if gitignore works

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -1,9 +1,18 @@
 import prod_image from './image/prod_image.jpeg'
+import test2_image from './image/test2.jpeg'
+
 let data_product = [
   {
     id: 1,
     name: 'Product',
     image: prod_image,
+    new_price: 50.0,
+    old_price: 80.0,
+  },
+  {
+    id: 2,
+    name: 'Product-2',
+    image: test2_image,
     new_price: 50.0,
     old_price: 80.0,
   },
